1aa3831c0bf2e337df352b9e6640dff91c97e799
Cyrille Favreau <cyrille.favreau@epfl.ch>
2021-04-20 19:32:28 +0200

    Version 1.0.0

--- bioexplorer/pythonsdk/bioexplorer/version.py
+++ bioexplorer/pythonsdk/bioexplorer/version.py
@@ -21,4 +21,4 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <https://www.gnu.org/licenses/>.
 
-VERSION = '0.9.2'
+VERSION = '1.0.0'
