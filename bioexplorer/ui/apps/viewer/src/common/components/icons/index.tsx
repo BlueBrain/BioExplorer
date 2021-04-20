bb9bf4bf73c434d2c77384389d3936d2d93fa115
Cyrille Favreau <cyrille.favreau@epfl.ch>
2021-04-20 11:52:21 +0200

    Copied UI from favreau/Brayns fork

---
+++ bioexplorer/ui/apps/viewer/src/common/components/icons/index.tsx
@@ -0,0 +1,5 @@
+export {GithubIcon} from './github';
+export {LightbulbFull} from './lightbulb-full';
+export {LightbulbOutline} from './lightbulb-outline';
+export {OwlIcon} from './owl';
+export {VectorSquareIcon} from './vector-square';
