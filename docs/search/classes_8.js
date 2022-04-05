20ea07d0744b8536931da5933f98d1c0dcae7c23
Cyrille Favreau <cyrille.favreau@epfl.ch>
2022-04-05 12:34:55 +0200

    README update with enzyme reactions and notebooks clean-up

--- docs/search/classes_8.js
+++ docs/search/classes_8.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['idsdetails_443',['IdsDetails',['../d1/d91/structbioexplorer_1_1details_1_1IdsDetails.html',1,'bioexplorer::details']]],
-  ['inspectiondetails_444',['InspectionDetails',['../dd/dc0/structbioexplorer_1_1details_1_1InspectionDetails.html',1,'bioexplorer::details']]]
+  ['idsdetails_442',['IdsDetails',['../d1/d91/structbioexplorer_1_1details_1_1IdsDetails.html',1,'bioexplorer::details']]],
+  ['inspectiondetails_443',['InspectionDetails',['../dd/dc0/structbioexplorer_1_1details_1_1InspectionDetails.html',1,'bioexplorer::details']]]
 ];
--- docs/search/classes_e.js
+++ docs/search/classes_e.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['residuesequence_481',['ResidueSequence',['../d0/df4/structbioexplorer_1_1molecularsystems_1_1ResidueSequence.html',1,'bioexplorer::molecularsystems']]],
-  ['response_482',['Response',['../db/df4/structbioexplorer_1_1mediamaker_1_1Response.html',1,'bioexplorer::mediamaker::Response'],['../d6/d45/structbioexplorer_1_1details_1_1Response.html',1,'bioexplorer::details::Response']]],
-  ['rgbcolordetails_483',['RGBColorDetails',['../df/dc7/structbioexplorer_1_1details_1_1RGBColorDetails.html',1,'bioexplorer::details']]],
-  ['rnasequence_484',['RNASequence',['../d0/d53/classbioexplorer_1_1molecularsystems_1_1RNASequence.html',1,'bioexplorer::molecularsystems']]],
-  ['rnasequencedetails_485',['RNASequenceDetails',['../d1/d00/structbioexplorer_1_1details_1_1RNASequenceDetails.html',1,'bioexplorer::details']]],
-  ['rnashape_486',['RNAShape',['../d4/dc3/classbioexplorer_1_1common_1_1RNAShape.html',1,'bioexplorer::common']]]
+  ['residuesequence_480',['ResidueSequence',['../d0/df4/structbioexplorer_1_1molecularsystems_1_1ResidueSequence.html',1,'bioexplorer::molecularsystems']]],
+  ['response_481',['Response',['../db/df4/structbioexplorer_1_1mediamaker_1_1Response.html',1,'bioexplorer::mediamaker::Response'],['../d6/d45/structbioexplorer_1_1details_1_1Response.html',1,'bioexplorer::details::Response']]],
+  ['rgbcolordetails_482',['RGBColorDetails',['../df/dc7/structbioexplorer_1_1details_1_1RGBColorDetails.html',1,'bioexplorer::details']]],
+  ['rnasequence_483',['RNASequence',['../d0/d53/classbioexplorer_1_1molecularsystems_1_1RNASequence.html',1,'bioexplorer::molecularsystems']]],
+  ['rnasequencedetails_484',['RNASequenceDetails',['../d1/d00/structbioexplorer_1_1details_1_1RNASequenceDetails.html',1,'bioexplorer::details']]],
+  ['rnashape_485',['RNAShape',['../d4/dc3/classbioexplorer_1_1common_1_1RNAShape.html',1,'bioexplorer::common']]]
 ];
